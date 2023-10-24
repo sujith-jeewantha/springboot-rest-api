b55cd06cdfb0d1c80432a4520306cf54cc6023f8
sujith-jeewantha <sujith.jeewantha@gmail.com>
2023-10-24 12:56:07 +0530

    Initial commit

---
+++ src/test/java/com/sujithjeewantha/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujithjeewantha.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
